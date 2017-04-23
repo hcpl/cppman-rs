4d1e6a36db60a2a1cea4eb418aecb39a49b28bd3
hcpl <hcpl.prog@gmail.com>
2017-04-23 15:25:03 +0300

    Add environment

---
+++ src/environ.rs
@@ -0,0 +1,65 @@
+use std::env;
+use std::fs::create_dir_all;
+use std::path::PathBuf;
+
+use get_lib_path;
+use config::{Config, Source, Pager};
+
+
+pub struct Environ {
+    home: PathBuf,
+    man_dir: PathBuf,
+    config_dir: PathBuf,
+    config_file: PathBuf,
+    config: Config,
+
+    index_db_re: PathBuf,
+    index_db: PathBuf,
+
+    pager: Pager,
+    pager_config: PathBuf,
+    pager_script: PathBuf,
+
+    source: Source,
+}
+
+impl Environ {
+    fn new() -> Environ {
+        let home = env::home_dir().unwrap();
+
+        let man_dir = home.join(".local/share/man/");
+        let config_dir = home.join(".config/cppman/");
+        let config_file = config_dir.join("cppman.cfg");
+
+        let config = Config::new_from_file(&config_file);
+
+        let _ = create_dir_all(&config_dir);
+
+        let index_db_re = config_dir.join("index.db");
+        let index_db = if index_db_re.exists() { 
+            index_db_re.clone()
+        } else {
+            get_lib_path("index.db")
+        };
+
+        let pager = config.pager();
+        let pager_config = get_lib_path("cppman.vim");
+        let pager_script = get_lib_path("pager.sh");
+
+        let source = config.source();
+
+        Environ {
+            home: home,
+            man_dir: man_dir,
+            config_dir: config_dir,
+            config_file: config_file,
+            config: config,
+            index_db_re: index_db_re,
+            index_db: index_db,
+            pager: pager,
+            pager_config: pager_config,
+            pager_script: pager_script,
+            source: source,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,14 @@ extern crate either;
 extern crate ini;
 
 mod config;
+mod environ;
 
+use std::path::PathBuf;
+
+
+pub fn get_lib_path(s: &str) -> PathBuf {
+    PathBuf::from(s)
+}
 
 fn main() {
     println!("Hello, world!");
